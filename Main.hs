104fec5e47acfb4d8928476d2c145cfc7483d05b
Solomon Bothwell <ssbothwell@gmail.com>
2020-02-15 20:31:08 -0800

    snapshot

--- Main.hs
+++ Main.hs
@@ -6,59 +6,75 @@ main = putStrLn "Hello, Haskell!"
 -- NOTE: All Ints should be Nats. Should use smart constructors and not export
 -- data constructors.
 
-data Position = Position { _posX :: Int, _posY :: Int }
+newtype Natural = Natural Int
   deriving (Show, Eq, Ord)
+
+mkNat :: Int -> Maybe Natural
+mkNat (isPos -> x) = Natural <$> x
 
 isPos :: Int -> Maybe Int
 isPos x = if x > 0 then Just x else Nothing
 
-mkPosition :: Int -> Int -> Maybe Position
-mkPosition (isPos -> x) (isPos -> y) = Position <$> x <*> y
-
-data Hole = Hole { _holeRadius :: Int, _holeDepth :: Int, _holePosition :: Position}
+data Position = Position { _posX :: Natural, _posY :: Natural }
   deriving (Show, Eq, Ord)
 
-mkHole :: Int -> Int -> Position -> Maybe Hole
-mkHole (isPos -> rad) (isPos -> dep) pos = Hole <$> rad <*> dep <*> pure pos
+mkPosition :: Natural -> Natural -> Position
+mkPosition = Position
 
-data Panel = Panel { _panelWidth :: Int, _panelLength :: Int, _panelThickness :: Int, _panelHolePattern :: [Hole] }
+data Hole = Hole { _holeRadius :: Natural, _holeDepth :: Natural, _holePosition :: Position}
   deriving (Show, Eq, Ord)
 
-data CabinetDims = CabinetDims { _cabinetWidth :: Int, _cabinetHeight :: Int, _cabinetDepth :: Int, _cabinetThickness :: Int }
+mkHole :: Natural -> Natural -> Position -> Hole
+mkHole = Hole
+
+data Panel = Panel { _panelWidth :: Natural, _panelLength :: Natural, _panelThickness :: Natural, _panelHolePattern :: [Hole] }
+  deriving (Show, Eq, Ord)
+
+data CabinetDims = CabinetDims
+  { _cabinetWidth :: Natural
+  , _cabinetHeight :: Natural
+  , _cabinetDepth :: Natural
+  , _cabinetMaterialThickness :: Natural
+  }
 
 -- Cabinet Properties
 data Style = Overlay | HalfOverlay | Inset
 data BoxConstruction = FreeStanding | SharedWall
 data Indexing = System | Shifted
 data Base = IntegralToeKick | LevelingFeet
-newtype BaseHeight = BaseHeight Int
-newtype Gap = Gap Int
-newtype Reveal = Reveal Int
-newtype FrontOffest = FrontOffset Int
-newtype RearOffset = RearOffset Int
-newtype HingeOffset = HingeOffset Int -- Hinge Cup offset. Should also include cup size and depth?
+newtype BaseHeight = BaseHeight Natural
+newtype Gap = Gap Natural
+newtype Reveal = Reveal Natural
+newtype FrontOffest = FrontOffset Natural
+newtype RearOffset = RearOffset Natural
+newtype HingeOffset = HingeOffset Natural -- Hinge Cup offset. Should also include cup size and depth?
+newtype HoleOffset = HoleOffset Natural
 newtype HasCountertop = HasCountertop Bool
-newtype StrainerWidth = StrainerWidth Int
-newtype NumOfBoxes = NumOfBoxes Int
-data BoxWidth = BWRatio Int | BWExplicit Int
-newtype BoxWidths = BoxWidths [BoxWidth]
+newtype StrainerWidth = StrainerWidth Natural
+newtype NumOfBoxes = NumOfBoxes Natural
+newtype BoxWidths = BoxWidths [Natural]
 
 data CabinetSpec = CabinetSpec
-  { _dims :: CabinetDims -- NOTE: Should this be overall dims or per box??
-  , _hasCntr :: HasCountertop -- NOTE: Should all optional properties be delegated to a list?
-  , _boxCnstr :: BoxConstruction
-  , _base :: Base
+  { _dims :: CabinetDims -- Overall Dimensions
+  -- , _hasCntr :: HasCountertop -- NOTE: Should all optional properties be delegated to a list?
+  , _boxCnstr :: BoxConstruction       -- Assume FreeStanding
+  , _base :: Base                      -- Assume LevelFeet
   , _baseHeight :: BaseHeight
-  , _style :: Style
+  -- , _style :: Style                 -- Assume Overlay
   , _gap :: Gap
   , _reveal :: Reveal
-  , _starts :: Starts -- TODO: What is this again?
-  , _indexing :: Indexing
+  , _starts :: (HoleOffset, HoleOffset)
+  , _indexing :: Indexing              -- Assume System
   , _rearOffset :: RearOffset
-  , _strainerWidth :: StrainerWidth
+  --, _strainerWidth :: StrainerWidth
   , _boxWidths :: BoxWidths
   }
 
 data Label = Upright | RearUpright | Bottom | Strainer | Countertop | Face | Toekick
   deriving (Show, Eq)
 
+newtype Cabinet = Cabinet [(Label, Panel, Position)]
+type CabinetUnit = [Cabinet]
+
+genCabinets :: CabinetSpec -> CabinetUnit
+genCabinets = undefined
